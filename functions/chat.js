5e060bbe9d55a1119db4eed32ec74ba4b82fe864
weblabsaus <hogridersupercell123@gmail.com>
2024-07-09 20:09:08 +1000

    Update chat.js

--- functions/chat.js
+++ functions/chat.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const express = require('express');
-const cors = require('cors');
-const app = express();
 const port = process.env.PORT || 3000;
 
+// Enable CORS for all origins
 app.use(cors());
 app.use(express.json());
 
