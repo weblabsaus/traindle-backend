7db86171e351ca75ad0ed2afa1b6e2d0742543f4
weblabsaus <hogridersupercell123@gmail.com>
2024-07-09 18:04:06 +1000

    Update chat.js

--- functions/chat.js
+++ functions/chat.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const express = require('express');
+const cors = require('cors');
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
